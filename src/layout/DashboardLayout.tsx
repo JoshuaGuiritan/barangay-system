0d513f19506956f8ba13af44099e53be55e09bde
Joshua Guiritan <joshuaguiritan05@gmail.com>
2026-03-03 11:14:58 +0800

    Vim

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -32,6 +32,24 @@ function DashboardLayout() {
         <div className="relative flex-1 flex items-center justify-center">
           <Outlet />
         </div>
+        {/* Copyright Notice */}
+        <CopyrightNotice />
+        <div className="navbar bg-base-100 h-19 shrink-0" />
+        <div className="relative flex-1 flex items-center justify-center">
+          <Outlet />
+        </div>
+        <div className="navbar bg-base-100 h-19 shrink-0" />
+        <div className="relative flex-1 flex items-center justify-center">
+          <Outlet />
+        </div>
+        <div className="navbar bg-base-100 h-19 shrink-0" />
+        <div className="relative flex-1 flex items-center justify-center">
+          <Outlet />
+        </div>
+        <div className="navbar bg-base-100 h-19 shrink-0" />
+        <div className="relative flex-1 flex items-center justify-center">
+          <Outlet />
+        </div>
       </div>
       <div className="h-full w-full flex flex-col bg-base-200">
         <div className="navbar bg-base-100 h-19 shrink-0" />
